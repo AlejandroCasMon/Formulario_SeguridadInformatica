60b5c47e5bc874f8d9ac7f39f707d1ff685e1465
CasMonDavid <davidcm200803@gmail.com>
2024-08-30 01:40:52 -0700

    Avance en funcionalidades

--- formulario/src/Components/Cuestionario.jsx
+++ formulario/src/Components/Cuestionario.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Cuestionario.css';
 
 const Cuestionario = () => {
+    const [nombre, setNombre] = useState("");
+    const [paterno, setPaterno] = useState("");
+    const [materno, setMaterno] = useState("");
+    const [fecha, setFecha] = useState(null);
+    const [direccion, setDireccion] = useState("");
+    const [telefono, setTelefono] = useState(0);
+    const [correo, setCorreo] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    const registrarUsuario = () => {
+        if (nombre!=null && paterno!=null && materno!=null && fecha!=null && direccion!=null && telefono!=null && correo!=null && password!=null && passwordConfirm!=null) {
+            alert("Usuairo registrado con exito!!");
+        }else {
+            alert("Faltan campos por rellenar");
+        }
+    };
+
+    const validarFecha = (fechaSeleccionada) => {
+        var fechaActual = new Date();
+        var fechaEntrada = new Date(fecha);
+
+        if (fechaEntrada > fechaActual) {
+            return false;
+        }
+        return true;
+    }
 
     return(
         <div className="cuerpo">
@@ -12,75 +39,104 @@ const Cuestionario = () => {
                     <h2>Formulario de registro</h2>
                     <Form.Group className="mb-3" controlId="nombreId">
                         <Form.Label className="etiquetaName">Nombre/s</Form.Label>
-                        <Form.Control type="text" placeholder="Nombre" />
-                        <Form.Text className="text-muted">
-                        Esto esta muteado
-                        </Form.Text>
+                        <Form.Control
+                        type="text"
+                        placeholder="Nombre"
+                        onChange={(event) => {
+                            setNombre(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="paterno">
                         <Form.Label className="etiquetaName">Apellido paterno</Form.Label>
-                        <Form.Control type="email" placeholder="Apellido paterno" />
-                        <Form.Text className="text-muted">
-                        Esto esta muteado
-                        </Form.Text>
+                        <Form.Control
+                        type="email"
+                        placeholder="Apellido paterno"
+                        onChange={(event) => {
+                            setPaterno(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="materno">
                         <Form.Label className="etiquetaName">Apellido materno</Form.Label>
-                        <Form.Control type="email" placeholder="Apellido materno" />
-                        <Form.Text className="text-muted">
-                        Esto esta muteado
-                        </Form.Text>
+                        <Form.Control
+                        type="email"
+                        placeholder="Apellido materno"
+                        onChange={(event) => {
+                            setMaterno(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="fechaNac">
                         <Form.Label className="etiquetaName">Fecha de nacimiento</Form.Label>
-                        <Form.Control type="date" />
-                        <Form.Text className="text-muted">
-                        Esto esta muteado
-                        </Form.Text>
+                        <Form.Control
+                        type="date"
+                        onChange={(event) => {
+                            setFecha(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="direccion">
                         <Form.Label className="etiquetaName">Dirección</Form.Label>
-                        <Form.Control as="textarea" rows={3} placeholder="Dirección" />
-                        <Form.Text className="text-muted">
-                        Esto esta muteado
-                        </Form.Text>
+                        <Form.Control
+                        as="textarea"
+                        rows={3}
+                        placeholder="Dirección"
+                        onChange={(event) => {
+                            setDireccion(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="telefono">
                         <Form.Label className="etiquetaName">Telefono</Form.Label>
-                        <Form.Control type="number" placeholder="Telefono" />
-                        <Form.Text className="text-muted">
-                        Esto esta muteado
-                        </Form.Text>
+                        <Form.Control
+                        type="number"
+                        placeholder="Telefono"
+                        onChange={(event) => {
+                            setTelefono(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="correo">
                         <Form.Label className="etiquetaName">Correo electronico</Form.Label>
-                        <Form.Control type="email" placeholder="ejemplo@dominio.xx" />
-                        <Form.Text className="text-muted">
-                        Esto esta muteado
-                        </Form.Text>
+                        <Form.Control
+                        type="email"
+                        placeholder="ejemplo@dominio.xx"
+                        onChange={(event) => {
+                            setCorreo(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="password">
                         <Form.Label className="etiquetaName">Contraseña</Form.Label>
-                        <Form.Control type="password" placeholder="Contraseña" />
+                        <Form.Control
+                        type="password"
+                        placeholder="Contraseña"
+                        onChange={(event) => {
+                            setPassword(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="confirmPassword">
                         <Form.Label className="etiquetaName">Confirmar contraseña</Form.Label>
-                        <Form.Control type="password" placeholder="Confirmar contraseña" />
+                        <Form.Control
+                        type="password"
+                        placeholder="Confirmar contraseña"
+                        onChange={(event) => {
+                            setPasswordConfirm(event.target.value);
+                        }}
+                        />
                     </Form.Group>
 
-                    <Form.Group className="m-4 check" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Aceptar terminos y condiciones" />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit">
+                    <Button variant="primary" onClick={registrarUsuario}>
                         Registrar
                     </Button>
                 </Form>
